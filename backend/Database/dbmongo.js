7c555d3d5ec55f4d3475705e5419396a9de4baed
mns2017 <st.8178067@gmail.com>
2024-06-22 00:14:26 +0530

    dekho

--- backend/Database/dbmongo.js
+++ backend/Database/dbmongo.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_DB_URL);
+        await mongoose.connect(process.env.MONGO_DB_URI);
         console.log("data base is connected")
     } catch (error) {
         console.log("AN ERROR OCCURED WHILE CONNECTING WITH DATA BASE", error.message)
